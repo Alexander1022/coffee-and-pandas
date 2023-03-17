a4208ca7da70fa5066d50b51849ee60673022294
Xander <alexanderfjor@gmail.com>
2023-03-17 19:11:14 +0200

    Init v2
    Init v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
 
+function App() {
   return (
     <div>
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
