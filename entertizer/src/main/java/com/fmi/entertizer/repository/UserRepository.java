73ea329073a85ec2b471ab74550d2450a850d409
Violeta Kastreva <81370278+violeta-kastreva@users.noreply.github.com>
2023-03-18 06:30:50 +0200

    fixed getAll userRepository method

--- entertizer/src/main/java/com/fmi/entertizer/repository/UserRepository.java
+++ entertizer/src/main/java/com/fmi/entertizer/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.fmi.entertizer.repository;
 
 import com.fmi.entertizer.model.entity.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
@@ -12,6 +17,10 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findFirstById(Long id);
 
+
+    @Modifying
+    @Transactional
+    @Query(value = "select * from User ", nativeQuery = true)
     Optional<User> getAll();
 
     Optional<User> getUserById(Long id);
