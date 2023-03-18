49ab3a7b8d720141aa84538f3884c73a636f682d
Violeta Kastreva <81370278+violeta-kastreva@users.noreply.github.com>
2023-03-18 22:16:03 +0200

    friend methods updates

--- entertizer/src/main/java/com/fmi/entertizer/repository/FriendRepository.java
+++ entertizer/src/main/java/com/fmi/entertizer/repository/FriendRepository.java
@@ -1,10 +1,12 @@
 package com.fmi.entertizer.repository;
 
 import com.fmi.entertizer.model.entity.Friend;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    @Transactional
     void deleteByFirstUserIdAndSecondUserId(Long firstId, Long secondId);
 }
